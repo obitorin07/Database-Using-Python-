0b8fe07545947ce3833a7089eac4f6954169dfb2
obitorin07 <tommylover86@gmail.com>
2024-03-10 19:46:39 +0530

    in this python file i create table using python in mysql

---
+++ Create_table.py
@@ -0,0 +1,38 @@
+from dotenv import dotenv_values
+import mysql.connector
+
+secret_code =dotenv_values('.secret_code')
+
+database_name=secret_code['DATABASE_NAME']
+host_name =secret_code['USER_HOST']
+user_password=secret_code['USER_PASSWORD']
+user_name=secret_code['USER_NAME']
+
+connect = mysql.connector.connect(user =user_name ,host=host_name,password=user_password,database=database_name)
+print("Contents of secret_code:", secret_code)
+#creating cursor
+cursor =connect.cursor()
+
+#now excecuting for create table 
+table_name ='CREATE TABLE STUDENT(NAME VARCHAR(255),ROLL_NO INT);'
+cursor.execute(table_name)
+
+connect.close()
+cursor.close()
+
+"""
+use LEARN_MYSQL;
+Reading table information for completion of table and column names
+You can turn off this feature to get a quicker startup with -A
+
+Database changed
+mysql> show tables;
++-----------------------+
+| Tables_in_LEARN_MYSQL |
++-----------------------+
+| STUDENT               |
++-----------------------+
+1 row in set (0.00 sec)
+
+"""
+
